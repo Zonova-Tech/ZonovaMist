c7b0467a7f82c8641be285ca9cb29b46a4e63606
zonova <malki@zonovasl.com>
2025-12-26 12:17:11 +0530

    Reverse API base URL logic as per review

--- lib/config.dart
+++ lib/config.dart
@@ -11,7 +11,7 @@ class AppConfig {
   static String get apiBaseUrl {
     // ✅ Always use Render for Web
     if (kIsWeb) {
-      return "http://localhost:5000/api";
+      return _renderBaseUrl;
     }
 
     // ✅ Always use Render for Production (release builds)
@@ -22,15 +22,15 @@ class AppConfig {
     // 🔹 for test locally again
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        return "http://192.168.1.10:5000/api"; // device on LAN
+        return "http://192.168.1.10:3000/api"; // device on LAN
       case TargetPlatform.iOS:
-        return "http://localhost:5000/api"; // iOS simulator
+        return "http://localhost:3000/api"; // iOS simulator
       case TargetPlatform.windows:
       case TargetPlatform.linux:
       case TargetPlatform.macOS:
-        return "http://localhost:5000/api"; // desktop
+      return "http://localhost:3000/api"; // desktop
       default:
-        return "http://localhost:5000/api";
+        return "http://localhost:3000/api";
     }
   }
 }
